cc37c7a2a021526bea0e4a5e068b1324d4f9a96d
ParanichevaAlyona <lalaalyona@bk.ru>
2024-12-16 13:25:52 +0300

    comments in import_data

--- import_data.py
+++ import_data.py
@@ -40,27 +40,27 @@ def read_data_equisize(no_intervals, interval_width, sorted_aps, act_map, log, d
 
     interval_timing = []
 
-    no_events_sums = 0
-    no_events_logs = 0
-    no_dfs = 0
+    no_events_sums = 0   # число событий всего в интервале
+    no_events_logs = 0   # число событий в записанном интервале лога
+    no_dfs = 0   #
 
     shadow_dict = {}
     shadow_event_l = 0
     logs_finished = 0
     log_progression = {}
-    for i in range(0, no_intervals):
+    for i in range(0, no_intervals): #проходим по каждому индексу интервала
         print('Interval ', i+1, '/', no_intervals)
-        lower_bound = i * interval_width
-        upper_bound = (i+1) * interval_width
+        lower_bound = i * interval_width  # номер самой первого перехода из интервала
+        upper_bound = (i+1) * interval_width # номер самого последнего перехода из интервала
         if i == (no_intervals - 1):
-            upper_bound = len(sorted_aps)
+            upper_bound = len(sorted_aps) # если у нас последний интервал, то последний переход это последняя пара из лога
 
-        dfs = sorted_aps[lower_bound:upper_bound]
-        no_dfs += len(dfs)
+        dfs = sorted_aps[lower_bound:upper_bound] # берем срез переходов ActivePair из текущего интервала
+        no_dfs += len(dfs) #текущее число обраотанных переходов
 
         print('#DFS:', len(dfs))
 
-        empty_mat = np.zeros([no_act, no_act], dtype=float)
+        empty_mat = np.zeros([no_act, no_act], dtype=float) #создаем пустую матрицу по числу активностей
 
         # For output
         filtered_events = {}
@@ -72,47 +72,48 @@ def read_data_equisize(no_intervals, interval_width, sorted_aps, act_map, log, d
         lowest = datetime(2050, 1, 1, tzinfo=pytz.UTC)
 
         log_dfs = {}
-        for df in dfs:
+        for df in dfs:  # идем по срезу пар из ActivePair
             if df.trace_no not in log_dfs.keys():
-                log_dfs[df.trace_no] = []
-            log_dfs[df.trace_no].append(df)
+                log_dfs[df.trace_no] = [] # добавляем все попадающиеся новые трейсы в словарь
+            log_dfs[df.trace_no].append(df) #добавляем в ключ трейса все его пары переходов ActivePair
 
-        for trace_no, dfss in log_dfs.items():
+        for trace_no, dfss in log_dfs.items(): # по порядку вытаскиевам трейс и все его попавшие переходы
             # print('\nTrace:', trace_no)
-            sorted_dfs = sorted(dfss)
+            sorted_dfs = sorted(dfss) # сортируем по времени все переходы в каждом трейсе
             filtered_events[trace_no] = []
-            for df in sorted_dfs:
+            for df in sorted_dfs: # каждая пара в отсортированном массиве пар текущего трейса
                 # print(df)
-                filtered_events[trace_no].append(df.event)
-                no_events_sums += 1
-            filtered_events[trace_no].append(sorted_dfs[len(sorted_dfs) - 1].event2)
+                filtered_events[trace_no].append(df.event) # упорядоченные пары добавляются в список
+                no_events_sums += 1 # число событий в трейсе
+            filtered_events[trace_no].append(sorted_dfs[len(sorted_dfs) - 1].event2) # так как перед этим добалялись 1ые события из пар,
+                # то последнее событие оказалось пропущенным, добавялем его (не синтетическое end)
 
             no_events_sums += 1
 
-        for trace_no, events in filtered_events.items():
+        for trace_no, events in filtered_events.items(): # добавляем синтетические start и end  в каждый трейс
             empty_mat[act_map['start'], act_map[events[0]['concept:name']]] += 1
             empty_mat[act_map[events[-1]['concept:name']], act_map['end']] += 1
 
         # Export filtered events to interval event logs
         new_log = EventLog()
         no_eve = 0
-        for t, trace in enumerate(log):
-            if i < 50:
+        for t, trace in enumerate(log): # идем по первоначальному логу по трейсам
+            if i < 50: # странная ситуевина, видимо что-то вспомогательное, не обращаем внимание
                 if t not in shadow_dict.keys():
                     shadow_dict[t] = []
             new_trace = Trace()
-            for trace_no, events in filtered_events.items():
-                if t == trace_no:
-                    for event in trace:
-                        if event in events:
-                            if event['time:timestamp'] < lowest:
+            for trace_no, events in filtered_events.items(): # извлекаем из списка с путями номер трейса(индекс списка) и список событий
+                if t == trace_no: # если номер трейса изначального лога совпал с номером трейса отфильтрованного лога
+                    for event in trace: # то для каждого события в первоначальном трейсе
+                        if event in events: # если событие из первоначального трейса попало в разделение по интервалам
+                            if event['time:timestamp'] < lowest: # отслеживаем минимум времени интервала и максимум
                                 lowest = event['time:timestamp']
                             if event['time:timestamp'] > highest:
                                 highest = event['time:timestamp']
-                            new_event = Event()
-                            new_event['concept:name'] = str(act_map[event['concept:name']])
-                            new_trace.append(new_event)
-                            no_events_logs += 1
+                            new_event = Event() # создаем событие
+                            new_event['concept:name'] = str(act_map[event['concept:name']]) # преобразуем название в число
+                            new_trace.append(new_event) # добавляем событие в трейс
+                            no_events_logs += 1 # инкрементируем число событий в интервале лога
                             no_eve += 1
                             if i < 50:
                                 shadow_dict[t].append(new_event)
@@ -127,12 +128,12 @@ def read_data_equisize(no_intervals, interval_width, sorted_aps, act_map, log, d
         print('#Events:', no_eve)
         print('#Events log:', no_events_logs)
         print('#Events shadow low:', shadow_event_l)
-        for act_pair in dfs:
+        for act_pair in dfs: # расчитываем матрицу переходов для текущего интервала
             a1 = act_map[act_pair.a1]
             a2 = act_map[act_pair.a2]
             empty_mat[a1, a2] += 1
 
-        dfg_time_matrix[i] = empty_mat
+        dfg_time_matrix[i] = empty_mat # добавляем в индекс интервала соответствующую ему матрицу переходов
 
         interval_timing.append((lowest, highest))
 
